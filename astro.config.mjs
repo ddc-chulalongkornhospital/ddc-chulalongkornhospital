5c3c5b777d7b71b6bb0c94249aa08c7e752411b8
cchaipat <34586349+cchaipat@users.noreply.github.com>
2024-07-04 18:40:47 +0700

    add commas...

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-  site: 'https//ddc-chulalongkornhospital.github.io'
-  base: 'ddc.chulalongkornhospital'
+  integrations: [tailwind()],
+  site: 'https//ddc-chulalongkornhospital.github.io',
+  base: 'ddc.chulalongkornhospital',
 });
